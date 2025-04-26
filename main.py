be9a577647fc8eeb393834ce8989a4cb59f0079a
Powahm <you@example.com>
2025-04-26 11:11:59 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,54 @@
+# To run this code you need to install the following dependencies:
+# pip install google-genai
+
+import base64
+from google import genai
+from google.genai import types
+
+import os
+from google import genai
+
+client = genai.Client(api_key=os.environ["GEMINI_API_KEY"])
+
+response = client.models.generate_content(
+    model="gemini-2.0-flash",
+    contents="Explain how AI works in a few words",
+)
+
+print(response.text)
+
+
+'''
+def generate():
+    client = genai.Client(
+        vertexai=True,
+        project="",
+        location="",
+    )
+
+    model = "gemini-2.5-flash-preview-04-17"
+    contents = [
+        types.Content(
+            role="user",
+            parts=[
+                types.Part.from_text(text="""INSERT_INPUT_HERE"""),
+            ],
+        ),
+    ]
+    generate_content_config = types.GenerateContentConfig(
+        response_mime_type="text/plain",
+        system_instruction=[
+            types.Part.from_text(text="""Give concise answers"""),
+        ],
+    )
+
+    for chunk in client.models.generate_content_stream(
+        model=model,
+        contents=contents,
+        config=generate_content_config,
+    ):
+        print(chunk.text, end="")
+
+if __name__ == "__main__":
+    generate()
+'''
